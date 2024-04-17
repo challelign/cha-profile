4aef4b9ecf0bcc5002bfae7daa52ff82073e1fe4
Challelign Tilahun <chalie2123@gmail.com>
2024-04-17 16:08:08 +0300

    update profile v2 fix email error

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,11 +62,16 @@ export default function Intro() {
 				initial={{ opacity: 0, y: 100 }}
 				animate={{ opacity: 1, y: 0 }}
 			>
-				<span className="font-bold">Hello, I'm Challelign.</span> I'm a{" "}
-				<span className="font-bold">full-stack developer</span> with{" "}
-				<span className="font-bold">4 years</span> of experience. I enjoy
+				<span className="font-bold ">Hello, I'm Challelign.</span>{" "}
+				<span className="font-bold text-[#fca5a5] ">
+					a full-stack developer
+				</span>{" "}
+				with <span className="font-bold">4 years</span> of experience. I enjoy
 				building <span className="italic">sites & apps</span>. My focus is{" "}
-				<span className="underline">React (Next.js) , MERN Stack</span>.
+				<span className="font-bold text-[#fca5a5] ">
+					Next.js, Laravel , MERN Stack
+				</span>{" "}
+				like Mongodb , Express ,React ,Nodejs, Prisma and more..
 			</motion.h1>
 
 			<motion.div
